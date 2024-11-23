d7638f6874198f0d651e8547c05bbb08ad2c92bf
HafizSantosa <myhafizas@gmail.com>
2024-11-23 10:35:35 +0700

    home & logs fragment

---
+++ app/src/main/java/com/example/ngantor/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.ngantor;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.ngantor.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Referensi tombol
+        Button btnHome = findViewById(R.id.btn_home);
+        Button btnLogs = findViewById(R.id.btn_logs);
+
+        // Set fragment default (Home)
+        replaceFragment(new HomeFragment());
+
+        // Tombol Home
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new HomeFragment());
+            }
+        });
+
+        // Tombol Logs
+        btnLogs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new LogsFragment());
+            }
+        });
+    }
+
+    // ganti fragment
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
