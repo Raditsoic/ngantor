785d9480fce38ca0c1900deaf0ae4e29d0901975
raditsoic <ruuoreo.o@gmail.com>
2024-11-27 23:52:11 +0700

    fix: db init

--- app/src/main/java/com/example/ngantor/data/database/AppDatabase.java
+++ app/src/main/java/com/example/ngantor/data/database/AppDatabase.java
@@ -6,12 +6,13 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.ngantor.data.models.SleepSession;
 import com.example.ngantor.data.models.SoundLog;
 import com.example.ngantor.data.repositories.dao.SleepSessionDao;
 import com.example.ngantor.data.repositories.dao.SoundLogDao;
 
 
-@Database(entities = {SoundLog.class}, version = 1, exportSchema = false)
+@Database(entities = {SoundLog.class, SleepSession.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     private static AppDatabase INSTANCE;
 
