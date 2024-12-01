821464d26b142c93dd09fccf560f86da53b390ad
raditsoic <ruuoreo.o@gmail.com>
2024-12-01 16:23:21 +0700

    fix: decibel sensor

--- app/src/main/java/com/example/ngantor/fragment/LogsFragment.java
+++ app/src/main/java/com/example/ngantor/fragment/LogsFragment.java
@@ -1,10 +1,12 @@
-package com.example.ngantor;
+package com.example.ngantor.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
+
+import com.example.ngantor.R;
 
 public class LogsFragment extends Fragment {
 
